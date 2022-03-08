de9df5d1d80a9c2737cb05e5c0b0c144bfb06c4e
tetunori <tetunori@gmail.com>
2022-03-08 23:27:28 +0900

    Add Timer Class

--- dist/v0.5.0/bmwalker.js
+++ dist/v0.5.0/bmwalker.js
@@ -1,3 +1,55 @@
 class BMWalker {
-  constructor() {}
+  static typeHuman = 0;
+  static typeCat = 1;
+  static typePigeon = 2;
+  static typeBox = 3;
+
+  // Constructor
+  constructor(type = BMWalker.typeHuman) {
+    this.type = type;
+
+    this.tm = new Timer();
+    this.starttime = this.tm.getTimer();
+  }
+
+  // API: Get markers
+  getMarkers(wh, tmsec = undefined) {
+    let markers = [];
+
+    if (tmsec === undefined) {
+      tmsec = this.tm.getTimer();
+    }
+    // console.log(tmsec);
+
+    // get markers
+    markers = [
+      { x: 0, y: 0 },
+      { x: 0, y: wh },
+      { x: wh / 2, y: wh },
+      { x: wh / 2, y: 0 },
+    ];
+
+    return markers;
+  }
+}
+
+class Timer {
+  // Constructor
+  constructor() {
+    const d = new Date().valueOf();
+    this.time = d;
+    this.start = d;
+
+    const precision = 10; // 10msec
+    setInterval(
+      function () {
+        this.time += precision;
+      }.bind(this),
+      precision
+    );
+  }
+
+  getTimer() {
+    return this.time - this.start;
+  }
 }
--- sample/sketch.js
+++ sample/sketch.js
@@ -1,12 +1,22 @@
 const W = 720;
 
+let bmw;
+
 function setup() {
   createCanvas(W, W);
+  bmw = new BMWalker(BMWalker.typeHuman);
 }
 
 function draw() {
   background(220);
 
-  // Draw texture
-}
+  const markers = bmw.getMarkers(200);
 
+  beginShape();
+  {
+    markers.forEach((m) => vertex(m.x, m.y));
+  }
+  endShape(CLOSE);
+
+  // noLoop();
+}
