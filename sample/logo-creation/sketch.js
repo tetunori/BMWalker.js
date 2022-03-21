6c714acf4670f6ed76c4e74c8b7a24404dc2aa65
tetunori <tetunori@gmail.com>
2022-03-21 12:13:41 +0900

    Complete logo creation

--- sample/logo-creation/sketch.js
+++ sample/logo-creation/sketch.js
@@ -3,10 +3,14 @@ const bmw = new BMWalker();
 
 function setup() {
   createCanvas(1280, 640);
+
+  textFont('Noto Sans JP');
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(220);
+
   drawWalkers();
   drawLogo();
 }
@@ -15,23 +19,28 @@ const drawWalkers = () => {
   // Get array of the current marker coordinates
   const walkerHeight = height * 0.34;
   const intervalTime = 300;
+  const rowWalkers = 3;
+  const columnWalkers = 13;
 
-  for (let j = 0; j < 3; j++) {
-    for (let i = 0; i < 16; i++) {
-      const azimuth = (10 * PI) / 16 + (i * PI) / 16;
-      const elevation = (PI / 4) * j - PI / 4;
+  for (let j = 0; j < rowWalkers; j++) {
+    for (let i = 0; i < columnWalkers; i++) {
+      if (!isWalkerDraw(i, j)) {
+        continue;
+      }
+      const azimuth = ((10 + i) * PI) / 16;
+      const elevation = (j - 1) * (PI / 4);
       bmw.setCameraParam(azimuth, 0, elevation);
-      const intervalX = i * 100 + 10;
-      const offsetY = walkerHeight * j - walkerHeight + height / 2;
+      const intervalX = i * 100 + 40;
+      const offsetY = walkerHeight * (j - 1) + height / 2;
+      const specTime = i * intervalTime + 20 * frameCount;
 
       // Draw lines
-      const lineMarkers = bmw.getLineMarkers(walkerHeight, i * intervalTime + 20 * frameCount);
+      const lineMarkers = bmw.getLineMarkers(walkerHeight, specTime);
       lineMarkers.forEach((m) => {
         line(m[0].x + intervalX, m[0].y + offsetY, m[1].x + intervalX, m[1].y + offsetY);
       });
 
-      const markers = bmw.getMarkers(walkerHeight, i * intervalTime + 20 * frameCount);
-      // console.log(markers);
+      const markers = bmw.getMarkers(walkerHeight, specTime);
 
       // Draw markers
       markers.forEach((m) => {
@@ -41,19 +50,51 @@ const drawWalkers = () => {
   }
 };
 
+const isWalkerDraw = (i, j) => {
+  let retValue = false;
+  if (frameCount < 120) {
+    // center walker only
+    if (i === 6 && j === 1) {
+      retValue = true;
+    }
+  } else if (frameCount < 170) {
+    // center 3 walkers only
+    if (j === 1) {
+      if (abs(i - 6) < 2) {
+        retValue = true;
+      }
+    }
+  } else if (frameCount < 220) {
+    // center row walkers only
+    if (j === 1) {
+      retValue = true;
+    }
+  } else {
+    // all of walkers
+    retValue = true;
+  }
+
+  return retValue;
+};
+
 const drawLogo = () => {
-  push()
-  noStroke();
+  if (frameCount < 270) {
+    return;
+  }
 
-  textFont('Noto Sans JP');
-  textAlign(CENTER, CENTER);
-  translate(0, height / 2);
+  push();
+  {
+    noStroke();
+    translate(0, height / 2);
 
-  const textOffsetY = 60;
-  textSize(120);
-  text('BMWalker.js', width / 2, -textOffsetY);
-  textSize(35);
-  text("A simple JavaScript library for biological motion 'Walker'", width / 2, textOffsetY);
-  noErase();
-  pop()
+    const textOffsetY = 60;
+    textSize(120);
+    text('BMWalker.js', width / 2, -textOffsetY);
+
+    textSize(35);
+    text("A simple JavaScript library for biological motion 'Walker'", width / 2, textOffsetY);
+    noErase();
+  }
+
+  pop();
 };
