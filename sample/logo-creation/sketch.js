a2a782a9fde659c29ccdf79ea70b85b39b938461
tetunori <tetunori@gmail.com>
2022-03-21 11:06:07 +0900

    update logo creation with simple logo
    but performance is still low

--- sample/logo-creation/sketch.js
+++ sample/logo-creation/sketch.js
@@ -1,30 +1,14 @@
 // Create Biological motion walker instance
 const bmw = new BMWalker();
 
-let logoGfx;
-let maskGfx;
-
 function setup() {
   createCanvas(1280, 640);
-  // pixelDensity(1)
-  logoGfx = createGraphics(width, height);
-  maskGfx = createGraphics(width, height);
-    fill(255);
-  stroke(0);
-  logoGfx.fill(0);
-  logoGfx.stroke(255);
-  prepareLogo();
 }
 
 function draw() {
   background(220);
-  logoGfx.background(0);
-
   drawWalkers();
-
-  const logoGfxPict = logoGfx.get();
-  logoGfxPict.mask(maskGfx);
-  image(logoGfxPict, 0, 0);
+  drawLogo();
 }
 
 const drawWalkers = () => {
@@ -44,7 +28,6 @@ const drawWalkers = () => {
       const lineMarkers = bmw.getLineMarkers(walkerHeight, i * intervalTime + 20 * frameCount);
       lineMarkers.forEach((m) => {
         line(m[0].x + intervalX, m[0].y + offsetY, m[1].x + intervalX, m[1].y + offsetY);
-        logoGfx.line(m[0].x + intervalX, m[0].y + offsetY, m[1].x + intervalX, m[1].y + offsetY);
       });
 
       const markers = bmw.getMarkers(walkerHeight, i * intervalTime + 20 * frameCount);
@@ -53,24 +36,24 @@ const drawWalkers = () => {
       // Draw markers
       markers.forEach((m) => {
         circle(m.x + intervalX, m.y + offsetY, 6);
-        logoGfx.circle(m.x + intervalX, m.y + offsetY, 6);
       });
     }
   }
 };
 
-const prepareLogo = () => {
-  maskGfx.textFont('Noto Sans JP');
-  maskGfx.textAlign(CENTER, CENTER);
-  maskGfx.translate(0, height / 2);
+const drawLogo = () => {
+  push()
+  noStroke();
+
+  textFont('Noto Sans JP');
+  textAlign(CENTER, CENTER);
+  translate(0, height / 2);
 
   const textOffsetY = 60;
-  maskGfx.textSize(120);
-  maskGfx.text('BMWalker.js', width / 2, -textOffsetY);
-  maskGfx.textSize(35);
-  maskGfx.text(
-    "A simple JavaScript library for biological motion 'Walker'",
-    width / 2,
-    textOffsetY
-  );
+  textSize(120);
+  text('BMWalker.js', width / 2, -textOffsetY);
+  textSize(35);
+  text("A simple JavaScript library for biological motion 'Walker'", width / 2, textOffsetY);
+  noErase();
+  pop()
 };
