bff9e6a249c4badcbade76b30800b380ed916d26
tetunori <tetunori@gmail.com>
2022-03-25 19:02:24 +0900

    Add template and app-01

---
+++ sample/app-01/sketch.js
@@ -0,0 +1,121 @@
+const W = 720;
+
+let myP5MovRec;
+let startTime;
+let walkerParamMatrix = [];
+
+// Biological motion walker instance
+const bmw = new BMWalker();
+
+function setup() {
+  createCanvas(W, W);
+  stroke(255);
+
+  prepareTexture(width, height);
+
+  initialize();
+
+  // Set camera
+  const azimuth = PI / 4;
+  const elevation = PI / 4;
+  bmw.setCameraParam(azimuth, 0, elevation);
+
+  // Set translation enabled
+  bmw.setTranslationParam(true);
+
+  // myP5MovRec = new P5MovRec();
+  // myP5MovRec.startRec();
+}
+
+const initialize = () => {
+  bmw.resetTimer();
+  startTime = Date.now();
+
+  walkerParamMatrix = [];
+  for (let i = 0; i < 13; i++) {
+    walkerParamMatrix.push({ time: random(-1200, 1200), pos: -250 + i * 50 });
+  }
+};
+
+function draw() {
+  background(30);
+
+  // Threshold lines
+  [112, -107].forEach((v) => {
+    push();
+    line(v, W + v, W + v, v);
+    pop();
+  });
+
+  const walkerHeight = 150;
+
+  const dressedTiming = 2600;
+  const undressedTiming = 5900;
+  const currentTime = Date.now() - startTime;
+
+  walkerParamMatrix.forEach((o) => {
+    const offsetY = walkerHeight / 2;
+    push();
+    {
+      translate(-o.pos, o.pos);
+      const cT = currentTime + o.time;
+
+      // Always, draw lines 
+      drawWalkerLines(offsetY, walkerHeight, cT);
+
+      // Draw markers
+      if (cT < dressedTiming) {
+        drawWalkerMarkers(offsetY, walkerHeight, dressedTiming);
+      } else if (cT > undressedTiming) {
+        drawWalkerMarkers(offsetY, walkerHeight, undressedTiming);
+      } else {
+        drawWalkerMarkers(offsetY, walkerHeight, cT);
+      }
+    }
+    pop();
+  });
+
+  // Reset
+  if (currentTime > 11000) {
+    initialize();
+  }
+
+  // Draw texture
+  image(textureGfx, 0, 0);
+}
+
+const drawWalkerLines = (offsetY, walkerHeight, tmsec = undefined) => {
+  const lineMarkers = bmw.getLineMarkers(walkerHeight, tmsec);
+
+  // Draw lines
+  lineMarkers.forEach((m) => {
+    line(m[0].x, m[0].y + offsetY, m[1].x, m[1].y + offsetY);
+  });
+};
+
+const drawWalkerMarkers = (offsetY, walkerHeight, tmsec = undefined) => {
+  const markers = bmw.getMarkers(walkerHeight, tmsec);
+
+  // Draw markers
+  markers.forEach((m) => {
+    circle(m.x, m.y + offsetY, 4);
+  });
+};
+
+function keyPressed() {
+  switch (keyCode) {
+    case 49: //1: Start record
+      myP5MovRec.startRec();
+      break;
+    case 50: //2: set webm, stop
+      // myP5MovRec.setMovType(P5MovRec.movTypeId.webm); // webm is default value
+      myP5MovRec.stopRec();
+      break;
+    case 51: //3: set mp4, stop
+      myP5MovRec.setMovType(P5MovRec.movTypeId.mp4); // for mp4 container
+      myP5MovRec.stopRec();
+      break;
+    default:
+      break;
+  }
+}
---
+++ sample/app-template/sketch.js
@@ -0,0 +1,62 @@
+let myP5MovRec;
+
+// Color picking
+const colsURL = 'https://coolors.co/ffbe0b-fb5607-ff006e-8338ec-3a86ff';
+let colors = createCols(colsURL);
+
+// Take some Colors from coolors URL
+function createCols(url) {
+  let slaIndex = url.lastIndexOf('/');
+  let colStr = url.slice(slaIndex + 1);
+  let colArr = colStr.split('-');
+  for (let i = 0; i < colArr.length; i++) colArr[i] = '#' + colArr[i];
+  return colArr;
+}
+
+const W = 720;
+
+// Biological motion walker instance
+const bmw = new BMWalker();
+
+function setup() {
+  createCanvas(W, W);
+  shuffle(colors, true);
+
+  prepareTexture(width, height);
+
+  myP5MovRec = new P5MovRec(); // P5MovRec.codecId.vp9 is selected by default.
+  myP5MovRec.startRec();
+}
+
+function draw() {
+  background(30);
+
+  const walkerHeight = 200;
+  const markers = bmw.getMarkers(walkerHeight);
+
+  // Draw markers
+  markers.forEach((m) => {
+    circle(m.x + W / 2, m.y + W / 2, 6);
+  });
+
+  // Draw texture
+  image(textureGfx, 0, 0);
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 49: //1: Start record
+      myP5MovRec.startRec();
+      break;
+    case 50: //2: set webm, stop
+      // myP5MovRec.setMovType(P5MovRec.movTypeId.webm); // webm is default value
+      myP5MovRec.stopRec();
+      break;
+    case 51: //3: set mp4, stop
+      myP5MovRec.setMovType(P5MovRec.movTypeId.mp4); // for mp4 container
+      myP5MovRec.stopRec();
+      break;
+    default:
+      break;
+  }
+}
