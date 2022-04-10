1bc16790c35d954122a96283fe5112076d1bd636
tetunori <tetunori@gmail.com>
2022-04-10 22:22:35 +0900

    Update logo creation sample

--- sample/logo-creation/sketch.js
+++ sample/logo-creation/sketch.js
@@ -1,3 +1,6 @@
+
+// let myP5MovRec;
+
 // Create Biological motion walker instance
 const bmw = new BMWalker();
 
@@ -6,6 +9,10 @@ function setup() {
 
   textFont('Noto Sans JP');
   textAlign(CENTER, CENTER);
+
+  // myP5MovRec = new P5MovRec(); // P5MovRec.codecId.vp9 is selected by default.
+  // myP5MovRec.startRec();
+
 }
 
 function draw() {
@@ -98,3 +105,21 @@ const drawLogo = () => {
 
   pop();
 };
+
+// function keyPressed() {
+//   switch (keyCode) {
+//     case 49: //1: Start record
+//       myP5MovRec.startRec();
+//       break;
+//     case 50: //2: set webm, stop
+//       // myP5MovRec.setMovType(P5MovRec.movTypeId.webm); // webm is default value
+//       myP5MovRec.stopRec();
+//       break;
+//     case 51: //3: set mp4, stop
+//       myP5MovRec.setMovType(P5MovRec.movTypeId.mp4); // for mp4 container
+//       myP5MovRec.stopRec();
+//       break;
+//     default:
+//       break;
+//   }
+// }
