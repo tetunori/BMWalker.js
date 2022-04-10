208d2f9b28c60c02909ae950d513cfbed4a9d584
tetunori <tetunori@gmail.com>
2022-04-10 15:33:17 +0900

    Support mouse operation on app-06

--- sample/app-06/gamepad.js
+++ sample/app-06/gamepad.js
@@ -45,28 +45,44 @@ class MyGamepad {
     pop();
   }
 
+  getLeftAxis() {
+    let leftAxis = undefined;
+
+    const gpi = this.gamepadIndex;
+    if (mouseIsPressed){
+      const leftAxisX = map(mouseX, 0, width, -1, 1 );
+      const leftAxisY = map(mouseY, 0, height, -1, 1 )
+      leftAxis = {x: leftAxisX, y: leftAxisY};
+    }else if (gpi !== undefined ) {
+      const gamePad = navigator.getGamepads()[gpi];
+
+      const GAMEPAD_LEFT_AXIS_X = 0;
+      const GAMEPAD_LEFT_AXIS_Y = 1;
+      const leftAxisX = gamePad.axes[GAMEPAD_LEFT_AXIS_X];
+      const leftAxisY = gamePad.axes[GAMEPAD_LEFT_AXIS_Y];
+      leftAxis = {x: leftAxisX, y: leftAxisY};
+    } 
+
+    return leftAxis;
+
+  }
+
   drawInputStatus(centerX, centerY, size) {
     push();
     {
       fill(this.statusLineC);
       stroke(this.statusLineC);
 
-      const gpi = this.gamepadIndex;
-      if (gpi !== undefined) {
-        const gamePad = navigator.getGamepads()[gpi];
-
-        const GAMEPAD_LEFT_AXIS_X = 0;
-        const GAMEPAD_LEFT_AXIS_Y = 1;
-        const leftAxisX = gamePad.axes[GAMEPAD_LEFT_AXIS_X];
-        const leftAxisY = gamePad.axes[GAMEPAD_LEFT_AXIS_Y];
-
-        const xPos = centerX + (leftAxisX * size) / 2;
-        const yPos = centerY + (leftAxisY * size) / 2;
+      const la = this.getLeftAxis();
+      if(la){
+        const xPos = centerX + (la.x * size) / 2;
+        const yPos = centerY + (la.y * size) / 2;
         line(xPos, yPos, centerX, centerY);
         circle(xPos, yPos, 6);
       } else {
         circle(centerX, centerY, 6);
       }
+
     }
     pop();
   }
@@ -74,15 +90,9 @@ class MyGamepad {
   getAngle() {
     let angle = 0;
 
-    const gpi = this.gamepadIndex;
-    if (gpi !== undefined) {
-      const gamePad = navigator.getGamepads()[gpi];
-
-      const GAMEPAD_LEFT_AXIS_X = 0;
-      const GAMEPAD_LEFT_AXIS_Y = 1;
-      const leftAxisX = gamePad.axes[GAMEPAD_LEFT_AXIS_X];
-      const leftAxisY = gamePad.axes[GAMEPAD_LEFT_AXIS_Y];
-      const v = createVector(leftAxisX, leftAxisY);
+    const la = this.getLeftAxis();
+    if(la){
+      const v = createVector(la.x, la.y);
       angle = v.heading();
       // console.log(angle);
     }
@@ -93,15 +103,9 @@ class MyGamepad {
   getMagnitude() {
     let mag = 0;
 
-    const gpi = this.gamepadIndex;
-    if (gpi !== undefined) {
-      const gamePad = navigator.getGamepads()[gpi];
-
-      const GAMEPAD_LEFT_AXIS_X = 0;
-      const GAMEPAD_LEFT_AXIS_Y = 1;
-      const leftAxisX = gamePad.axes[GAMEPAD_LEFT_AXIS_X];
-      const leftAxisY = gamePad.axes[GAMEPAD_LEFT_AXIS_Y];
-      const v = createVector(leftAxisX, leftAxisY);
+    const la = this.getLeftAxis();
+    if(la){
+      const v = createVector(la.x, la.y);
       mag = min(1.0, v.mag());
       // console.log(mag);
     }
