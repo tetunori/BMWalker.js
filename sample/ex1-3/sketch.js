4966f9c7f4455f4c6a722be283a670e35cb28bfc
tetunori <tetunori@gmail.com>
2022-04-20 21:24:27 +0900

    Add zpos sample

---
+++ sample/ex1-3/sketch.js
@@ -0,0 +1,28 @@
+const W = 360;
+
+// Biological motion walker instance
+const bmw = new BMWalker();
+
+function setup() {
+  createCanvas(W, W, WEBGL);
+  debugMode();
+}
+
+function draw() {
+  background(220);
+  orbitControl();
+
+  // Get current markers
+  const walkerHeight = 200;
+  const markers = bmw.getMarkers(walkerHeight);
+
+  // Draw markers
+  markers.forEach((m) => {
+    push();
+    {
+      translate(m.x, m.y, m.z);
+      sphere(2);
+      pop();
+    }
+  });
+}
